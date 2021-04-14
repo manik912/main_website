6bd26bac83fcd3e1f88f4c50b51545b0a1779491
Manik <guptamanik1921@gmail.com>
2021-04-14 23:43:59 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib import messages
+# Create your views here.
+
+
+def register(request):
+    if(request.method == 'POST'):
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            messages.add_message(request, messages.INFO, 'Thank You for registering!')
+            form.save()
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form' : form,
+    }
+    return render(request, 'home/index.html',context)
+
+
